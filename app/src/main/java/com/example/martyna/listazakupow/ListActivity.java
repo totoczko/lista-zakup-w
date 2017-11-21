d63cda5f93fa35fb3d947a02af4fa4e2d99234e9
totoczko <m.toloczko1@gmail.com>
2017-11-21 20:57:46 +0100

    wyswietlanie z bazy danych

--- app/src/main/java/com/example/martyna/listazakupow/ListActivity.java
+++ app/src/main/java/com/example/martyna/listazakupow/ListActivity.java
@@ -24,14 +24,47 @@ public class ListActivity extends parentActivity {
         // Create and/or open a database to read from it
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
-        // Perform this raw SQL query "SELECT * FROM pets"
-        // to get a Cursor that contains all rows from the pets table.
-        Cursor cursor = db.rawQuery("SELECT * FROM " + groceryContract.GroceryEntry.TABLE_NAME, null);
+        //reading from db
+        //which column are we interested in
+        String[] projection = {
+                groceryContract.GroceryEntry._ID,
+                groceryContract.GroceryEntry.COLUMN_NAME_TITLE,
+                groceryContract.GroceryEntry.COLUMN_NAME_PRICE,
+                groceryContract.GroceryEntry.COLUMN_NAME_QUANTITY
+        };
+
+        //selection, stectionArgs - which row are we interested in
+
+        Cursor cursor = db.query(
+                groceryContract.GroceryEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        //display the list items
         try {
             // Display the number of rows in the Cursor (which reflects the number of rows in the
             // pets table in the database).
             TextView displayView = (TextView) findViewById(R.id.checkDB);
             displayView.setText("Number of rows in grocery database table: " + cursor.getCount());
+
+            int idColumnIndex = cursor.getColumnIndex(groceryContract.GroceryEntry._ID);
+            int titleColumnIndex = cursor.getColumnIndex(groceryContract.GroceryEntry.COLUMN_NAME_TITLE);
+            int priceColumnIndex = cursor.getColumnIndex(groceryContract.GroceryEntry.COLUMN_NAME_PRICE);
+            int quantityColumnIndex = cursor.getColumnIndex(groceryContract.GroceryEntry.COLUMN_NAME_QUANTITY);
+
+            while(cursor.moveToNext()){
+                int currentID = cursor.getInt(idColumnIndex);
+                String currentTitle = cursor.getString(titleColumnIndex);
+                String currentPrice = cursor.getString(priceColumnIndex);
+                int currentQuantity = cursor.getInt(quantityColumnIndex);
+
+                displayView.append("\n" + currentID + " - " + currentTitle + " - " + currentPrice + " - " + currentQuantity);
+            }
+
         } finally {
             // Always close the cursor when you're done reading from it. This releases all its
             // resources and makes it invalid.
